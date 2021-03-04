95dc935cc76f48c206e980f89da068e6c3f0842f
xiaobaifeng <835277213@qq.com>
2021-03-04 14:33:27 +0800

    chore: 引入es2020特性

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  plugins: [
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-private-methods'
   ]
 }
